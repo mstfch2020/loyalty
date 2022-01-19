0c3df0c0051cd7a7f7ad5d20df43463504040824
Mahdi.Dev <@pec10402@mm>
2022-01-19 15:22:01 +0330

    use multi select in forms

--- src/app/views/pages/admin/customer-group/customer-group.component.ts
+++ src/app/views/pages/admin/customer-group/customer-group.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerGroupComponent implements OnInit {
 
+  selectedCar = 1;
+
+  cars = [
+    { id: 1, name: 'زینجا' },
+    { id: 2, name: 'مون' },
+    { id: 3, name: 'قهر کرده' },
+    { id: 4, name: '09192935850' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/views/pages/admin/send-sms/send-sms.component.ts
+++ src/app/views/pages/admin/send-sms/send-sms.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendSmsComponent implements OnInit {
 
+
+  selectedCar = 1;
+
+  cars = [
+    { id: 1, name: 'زینجا' },
+    { id: 2, name: 'مون' },
+    { id: 3, name: 'قهر کرده' },
+    { id: 4, name: '09192935850' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
