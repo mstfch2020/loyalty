284a76f9e05d6b19ce0c2d387d20be55d66e0fea
Mahdi.Dev <@pec10402@mm>
2022-01-21 17:50:19 +0330

    add modal

--- src/app/views/pages/admin/system-settings/system-settings-discount/system-settings-discount-grid/system-settings-discount-grid.component.ts
+++ src/app/views/pages/admin/system-settings/system-settings-discount/system-settings-discount-grid/system-settings-discount-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-system-settings-discount-grid',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemSettingsDiscountGridComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string = '';
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Write code on Method
+   * @return response()
+   */
+  open(content: any) {
+    this.modalService.open(content, {size: 'lg', backdrop: 'static',ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  /**
+   * Close all displayed modal
+   */
+  close(){
+    this.modalService.dismissAll();
+  }
+
+  /**
+   * Write code on Method
+   * @return response()
+   */
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
