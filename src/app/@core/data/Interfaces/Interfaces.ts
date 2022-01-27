cda95669dae27c6ad127561043afeef7490073f0
Mahdi.Dev <@pec10402@mm>
2022-01-27 20:11:26 +0330

    responsive scenario/1 form

---
+++ src/app/@core/data/Interfaces/Interfaces.ts
@@ -0,0 +1,16 @@
+export interface IMenuItem {
+
+  title: string;
+  url: string;
+  visible: boolean;
+}
+
+export interface IUser {
+
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+  avatar: string;
+}
--- src/app/views/pages/admin/main/main.component.ts
+++ src/app/views/pages/admin/main/main.component.ts
@@ -1,38 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { Subscription } from "rxjs";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {IMenuItem} from "src/app/@core/data/Interfaces/Interfaces";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit
-{
+export class MainComponent implements OnInit {
 
   paramsSubscription: Subscription | undefined;
 
   public isToggle: boolean;
+  public menuItems: IMenuItem[];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-  )
-  {
+  ) {
     this.isToggle = false;
+    this.menuItems=[
+      {
+        title:'سناریوها',
+        url:'/admin/main/scenario',
+        visible:true
+      },
+      {
+        title:'گروه مشتری',
+        url:'/admin/main/customergroup',
+        visible:true
+      },
+      {
+        title:'گروه مشتری موقت',
+        url:'/admin/main/customergrouptemporary',
+        visible:true
+      },
+      {
+        title:'مشتریان',
+        url:'/admin/main/customer',
+        visible:true
+      },
+      {
+        title:'کاربران پنل',
+        url:'/admin/main/users',
+        visible:true
+      },
+      {
+        title:'ارسال پیامک',
+        url:'/admin/main/sms',
+        visible:true
+      },
+      {
+        title:'گزارش ها',
+        url:'/admin/main/reports',
+        visible:true
+      },
+      {
+        title:'تنظیمات سیستمی',
+        url:'/admin/main/settings',
+        visible:true
+      }
+    ]
   }
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.paramsSubscription = this.route.params.subscribe(
-      (params: Params) =>
-      {
+      (params: Params) => {
         //this.router.navigate(['/admin/profile']);
       });
   }
 
-  OnDestroy()
-  {
+  OnDestroy() {
     this.paramsSubscription?.unsubscribe();
   }
 
