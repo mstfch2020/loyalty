9de91e93d066cdc49585d13bb90b57a8f405c498
M.Chitsazan <M.Chitsazan@NGN-HP46>
2022-03-27 12:51:25 +0430

    fix alert bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgSelectConfig } from '@ng-select/ng-select';
 import { RootStoreService } from './@core/services/root-store.service';
+import { AlertService } from './@core/services/ui/alert.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,9 @@ export class AppComponent
   title = 'loyalty';
   isLoading = false;
 
-  constructor(public rootStoreService: RootStoreService, private config: NgSelectConfig)
+  constructor(public rootStoreService: RootStoreService, private router: Router, private alertService: AlertService, private config: NgSelectConfig)
   {
-
+    router.events.subscribe((val) => this.alertService.clearAllMessages());
     config.notFoundText = 'موردی یافت نشد.';
     rootStoreService.isLoading().subscribe(value =>
     {
