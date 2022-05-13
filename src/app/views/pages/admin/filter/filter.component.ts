a69b93cc1732c8c0370c6c6b004036cb9af23103
Mostafa chitsazan <mstf.ch2020@gmail.com>
2022-05-13 18:15:11 +0430

    fix bug

--- src/app/views/pages/admin/filter/filter.component.ts
+++ src/app/views/pages/admin/filter/filter.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output, HostListener } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { FilterTitle } from "src/app/@core/data/loyalty/get-senarios-grid.model";
 import { Utility } from 'src/app/@core/utils/Utility';
 
@@ -8,7 +8,8 @@ import { Utility } from 'src/app/@core/utils/Utility';
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit
+{
 
   @Input() title: string;
   @Input() isRadio = false;
@@ -18,50 +19,61 @@ export class FilterComponent implements OnInit {
   @Output() applyEvent: EventEmitter<any> = new EventEmitter<any>();
 
   filterForm!: FormGroup;
-  conditionType = 1;
   isChecked: boolean;
 
-  constructor() {
+  constructor()
+  {
     this.title = '';
     this.items = [];
     this.isChecked = false;
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.filterForm = new FormGroup({
       conditionType: new FormControl(null),
       searchValue: new FormControl(null)
     });
   }
 
-  cancelEventNotify() {
+  cancelEventNotify()
+  {
     this.cancelEvent.emit(false);
   }
 
-  applyEventNotify() {
+  applyEventNotify()
+  {
 
     let searchValue = this.filterForm.get('searchValue')?.value;
 
-    if (searchValue && new RegExp(Utility.mobileRegEx).test(searchValue)) {
-      this.applyEvent.emit({ value: [{ id: searchValue, title: searchValue, type: 3 }], conditionType: this.conditionType });
-    } else {
-      this.applyEvent.emit({ value: this.items.filter(p => p.checked), conditionType: this.conditionType });
+    if (searchValue && new RegExp(Utility.mobileRegEx).test(searchValue))
+    {
+      this.applyEvent.emit({ value: [{ id: searchValue, title: searchValue, type: 3 }], conditionType: this.filterForm.get("conditionType")?.value });
+    } else
+    {
+      this.applyEvent.emit({ value: this.items.filter(p => p.checked), conditionType: this.filterForm.get("conditionType")?.value });
     }
 
     var theCheckedItems = this.items.filter(w => w.checked);
-    if (theCheckedItems.length > 0) {
+    if (theCheckedItems.length > 0)
+    {
       this.isChecked = true;
-    } else {
+    } else
+    {
       this.isChecked = false;
     }
 
     this.cancelEvent.emit(false);
   }
 
-  changed(item: FilterTitle) {
-    if (this.isRadio) {
-      this.items.forEach(p => {
-        if (item.id === p.id) {
+  changed(item: FilterTitle)
+  {
+    if (this.isRadio)
+    {
+      this.items.forEach(p =>
+      {
+        if (item.id === p.id)
+        {
           p.checked = true;
         }
         else { p.checked = false; }
@@ -71,9 +83,12 @@ export class FilterComponent implements OnInit {
     item.checked = !item.checked;
 
     //if (this.items.findIndex(p => p.id === 'all' && p.checked) !== -1)
-    if (item.id === 'all') {
-      this.items.forEach(p => {
-        if (item.checked) {
+    if (item.id === 'all')
+    {
+      this.items.forEach(p =>
+      {
+        if (item.checked)
+        {
           p.checked = true;
         }
         else { p.checked = false; }
@@ -81,9 +96,11 @@ export class FilterComponent implements OnInit {
     }
   }
 
-  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent)
+  {
     let key = event.keyCode;
-    if (key == 27) {
+    if (key == 27)
+    {
       this.cancelEventNotify();
     }
   }
